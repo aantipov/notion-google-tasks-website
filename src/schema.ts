279051d9c0203b1432157f7f70f5164b3556aab1
Alexey Antipov <antipov.alexei@gmail.com>
2023-11-27 16:59:37 +0100

    schema improve

--- src/schema.ts
+++ src/schema.ts
@@ -1,6 +1,6 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 import type * as googleApi from '@/functions-helpers/google-api';
-import type * as notionApi from '@/functions-helpers/notion-api';
+import * as notionApi from '@/functions-helpers/notion-api';
 
 type GTokenT = ReturnType<typeof googleApi.fetchToken> extends Promise<infer T>
 	? T
@@ -9,10 +9,9 @@ type GTokenRestrictedT = Pick<GTokenT, 'user' | 'refresh_token'>;
 type NTokenT = ReturnType<typeof notionApi.fetchToken> extends Promise<infer T>
 	? T
 	: never;
-
-type gTaskId = string;
-type nTaskId = string;
-type completedAt = string | null; // ISO date string '2023-10-25'
+type GTaskIdT = string;
+type NTaskIdT = string;
+type CompletedAtT = string | null; // ISO date string '2023-10-25'
 
 export const users = sqliteTable('users', {
 	email: text('email').primaryKey(),
@@ -23,7 +22,7 @@ export const users = sqliteTable('users', {
 	tasklistId: text('tasklist_id'),
 	databaseId: text('database_id'),
 	mapping: text('mapping', { mode: 'json' }).$type<
-		[gTaskId, nTaskId, completedAt?][]
+		[GTaskIdT, NTaskIdT, CompletedAtT?][]
 	>(),
 	lastSynced: integer('last_synced', { mode: 'timestamp' }), // Important to recognize that sync was established successfully
 	created: integer('created', { mode: 'timestamp' }).notNull(),
