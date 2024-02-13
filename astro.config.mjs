b48206a1548482789b2fffa64796d535f980e377
Alexey Antipov <antipov.alexei@gmail.com>
2024-02-13 13:08:22 +0100

    refactor: use astro static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,23 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://notion-google-tasks-sync.com',
-	output: 'hybrid',
-	adapter: cloudflare({
-		mode: 'directory',
-		routes: {
-			strategy: 'auto',
-			include: ['/google-auth/*', '/notion-auth/*', '/api/*'],
-		},
-		runtime: {
-			mode: 'local',
-			type: 'pages',
-			bindings: { DB: { type: 'd1' } },
-		},
-	}),
+	output: 'static',
+	// adapter: cloudflare({
+	// 	mode: 'directory',
+	// 	routes: {
+	// 		strategy: 'auto',
+	// 		include: ['/google-auth/*', '/notion-auth/*', '/api/*'],
+	// 	},
+	// 	runtime: {
+	// 		mode: 'local',
+	// 		type: 'pages',
+	// 		bindings: { DB: { type: 'd1' } },
+	// 	},
+	// }),
 	integrations: [
 		tailwind({
 			applyBaseStyles: false, // is needed because of the shadcn/ui
